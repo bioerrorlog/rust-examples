fede1ffde92c547695d6d89c4aa1090634b58a5d
bioerrorlog <bioerrorlog.contact@gmail.com>
2024-08-24 13:28:46 +0900

    error with old api

--- echors/src/main.rs
+++ echors/src/main.rs
@@ -1,9 +1,24 @@
-use clap::Command;
+use clap::{Arg, Command};
 
 fn main() {
-    let _matchs = Command::new("echoes")
+    let matchs = Command::new("echors")
         .version("0.1.0")
         .author("BioErrorLog <bioerrorlog.contact@gmail.com>")
         .about("Rust echo")
+        .arg(
+            Arg::new("text")
+                .value_name("TEXT")
+                .help("Input text")
+                .required(true)
+                .min_value(1),
+        )
+        .arg(
+            Arg::new("omit_newline")
+                .short('n')
+                .help("Do not print newline")
+                .takes_value(false),
+        )
         .get_matches();
+
+    println!("{:#?}", matchs)
 }
